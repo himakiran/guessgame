4a74475f5ae912b48d02a184c87a7d25ae3b0d3c
himakiran <himakiran@users.noreply.github.com>
2018-02-25 17:55:55 +0530

    deleted README.md and renamedand modified Guessgame.sh

--- Guessgame.sh
+++ Guessgame.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-#Guessinggame.sh
+#guessinggame.sh
 
+function initialize {
 a=$(ls -p | grep -v / | wc -l)
 response=$(($a-1))
+}
+initialize
 while [[ $response -ne $a ]]
 do
 echo "Guess the number of Files in the present directory"
@@ -11,6 +14,13 @@ if [[ $response -eq $a ]]
 then
 echo "Congrats "
 else
+if [[ $response -lt $a ]]
+then
+echo "Your guess is too low"
 echo "Try again"
+else
+echo "Your guess is too high"
+echo "Try again"
+fi
 fi
 done
